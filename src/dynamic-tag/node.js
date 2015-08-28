9ba8f6dcd115b730aa0d2c787b070b0cae75e40b
Kristian Mandrup <kmandrup@gmail.com>
2015-08-28 11:31:54 +0100

    it works :)

--- src/dynamic-tag/node.js
+++ src/dynamic-tag/node.js
@@ -5,9 +5,44 @@ function DynamicTagNode(props) {
     }
 }
 DynamicTagNode.prototype = {
-    doGenerateCode: function (template) {
-      console.log(template.data);
-      this.generateCode(template)
+    generateTagName: function(name, template) {
+      var name = this.getAttribute('tag-name');
+      console.log('generateTagName', name);
+      // $yyy.xxx or ${yyy.xxx}
+      // -> ' + yyy.xxx + '
+      var expr1 = /\${\s*(\S+)\s*}/;
+      var expr2 = /\$\s*(\S+)\s*/;
+
+      var matchComplex = name.match(expr1);
+      var replStr = "%:$1%";
+      var nodeNameExpr = name.replace(expr1, replStr);
+      if (!matchComplex) {
+        nodeNameExpr = name.replace(expr2, replStr);
+      }
+      nodeNameExpr = nodeNameExpr || name;
+
+      if (nodeNameExpr.match(/%/)) {
+        console.log('write code');
+        name = nodeNameExpr;
+        var parts = name.split('%');
+        for (var part of parts) {
+          var code = part.slice(1);
+          code = 'out.w(' + code + ');';
+          var isCode = part[0] === ':';
+          if (isCode) {
+            this.addSkipAttribute('tag-name');
+            // delete this.attributesByNS['']['tag-name'];
+            // console.log();
+            template.indent();
+            template.code(code);
+          } else {
+            template.text(part)
+          }
+        }
+      } else {
+        name = nodeNameExpr;
+        template.text(name);
+      }
     }
 };
 
--- test/test.js
+++ test/test.js
@@ -8,8 +8,8 @@ describe('marko-dynamic-tag', function() {
     it('should work with a tag-name attribute that has a value', function() {
         var template = require('./fixtures/simple.marko');
         var output = template.renderSync({
-            lv: '1'
+            lv: '4'
         });
-        expect(output).to.equal("<h1 class='ui header'>hello world</h1>");
+        expect(output).to.equal("<h4 class=\"ui header\">hello world</h4>");
     });
 });
